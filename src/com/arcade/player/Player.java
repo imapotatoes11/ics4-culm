d19672c8b57c64bb24771ca670b6a086e8628ced
EggieNotEggs <eddie.chao.qu@gmail.com>
2025-06-02 09:40:48 -0400

    Create Player Class

---
+++ src/com/arcade/player/Player.java
@@ -0,0 +1,47 @@
+public class Player {
+   private String username;
+   private String password;
+   private int age;
+   private String name;
+   private int creditCardInfo;
+   
+   public String getUsername () {
+      return username;
+   }
+   
+   public void setUsername (String username) {
+      this.username = username;
+   }
+   
+   public String getPassword () {
+      return password;
+   }
+   
+   public void setPassword (String password) {
+      this.password = password;
+   }
+   
+   public int getAge () {
+      return age;
+   }
+   
+   public void setAge (int age) {
+      this.age = age;
+   }
+   
+   public String getName () {
+      return name;
+   }
+   
+   public void setName (String name) {
+      this.name = name;
+   }
+   
+   public int getCreditCardInfo () {
+      return creditCardInfo;
+   }
+   
+   public void setCreditCardInfo (int creditCardInfo) {
+      this.creditCardInfo = creditCardInfo;
+   }
+}
