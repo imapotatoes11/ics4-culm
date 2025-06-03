83c5a84cac5bce40d629eb83a4c3f37fe0364509
EggieNotEggs <eddie.chao.qu@gmail.com>
2025-06-03 10:52:51 -0400

    Create Question Class

---
+++ src/com/arcade/games/trivia/Question.java
@@ -0,0 +1,17 @@
+public class Question {
+   private String question;
+   private int difficultyLevel;
+   private char answer;
+   
+   public String getQuestion () {
+      return question;
+   } 
+   
+   public int getDifficultyLevel () {
+      return difficultyLevel;
+   }
+   
+   public char getAnswer () {
+      return answer;
+   }
+}
