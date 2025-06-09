68f235ebbb10bf34296764fe808c5a3597a592d4
Lenovo <wenzhengdong88@gmail.com>
2025-06-09 11:34:48 -0400

    resres

--- src/com/arcade/games/diceopoly/Diceopoly.java
+++ src/com/arcade/games/diceopoly/Diceopoly.java
@@ -1,47 +1,16 @@
-package com.arcade.games.diceopoly;
-
+package src.com.arcade.games.diceopoly;
 import java.util.*;
 import com.arcade.games.Game;
-import com.arcade.item.Functional;
 
-public class Diceopoly extends Game {
-    private int pos = 0; // The users position on the board
-    private int diceCount = 7; // The number of dice the user can roll
-    private int boardLength = 30; // The length of the board array
-    private String[] board = new String[boardLength]; // The board the user is on
-    private int dice; // Your dice roll
+public class Diceopoly{
+    private int pos = 0; //The users position on the board
+    private int diceCount = 7; //The number of dice the user can roll
+    private int boardLength = 30; //The length of the board array
+    private String[] board = new String[boardLength]; //The board the user is on
+    private int dice; //Your dice roll
     Scanner sc = new Scanner(System.in);
     Random rand = new Random();
 
-    // ANSI color codes for UI
-    public static final String RESET = "\u001B[0m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
-    public static final String PURPLE = "\u001B[35m";
-    public static final String CYAN = "\u001B[36m";
-
-    public Diceopoly(int id, String title, int difficulty, int requiredTokens, int ticketReward) {
-        super(id, title, difficulty, requiredTokens, ticketReward);
-    }
-
-    public Diceopoly() {
-        super(1, "Diceopoly", 3, 5, 10); // Example values for the constructor
-    }
-
-    public int runGame(ArrayList<Functional> useItems) {
-        System.out.println("Welcome to Diceopoly!");
-        System.out.println("You have " + diceCount + " dice to roll.");
-        System.out.println("Reach the end of the board to win!");
-
-        generateBoard();
-        printBoard();
-        moving();
-
-        return 0;
-        // TODO: RETURN TICKET REWARDS
-    }
 
     public static void main(String[] args) {
         Diceopoly game = new Diceopoly();
@@ -50,6 +19,7 @@ public class Diceopoly extends Game {
         game.moving();
         System.out.println("Game Over! You finished at position " + game.pos);
     }
+
 
     public void moving() {
         while (diceCount > 0 && pos < boardLength - 1) {
@@ -60,12 +30,12 @@ public class Diceopoly extends Game {
                 dice = rollDice();
                 System.out.println("You rolled a " + dice);
 
-                // Move one tile at a time
+                //Move one tile at a time
                 for (int step = 0; step < dice && pos < boardLength - 1; step++) {
                     pos++;
                     printBoard();
 
-                    // End early if somehow moved to last tile
+                    //End early if somehow moved to last tile
                     if (pos >= boardLength - 1) {
                         System.out.println("You reached the end of the board. You Win!");
                         return;
@@ -91,92 +61,69 @@ public class Diceopoly extends Game {
         System.out.println("Game Over! You finished at position " + pos);
     }
 
-    // Updated printBoard to clear screen, show status, highlight player, and color
-    // tiles
     public void printBoard() {
-        // Clear console
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-        System.out.println(String.format("Position: %d    Dice Left: %d", pos, diceCount));
-
-        StringBuilder view = new StringBuilder();
-        int start = Math.max(0, pos - 2);
-        int end = Math.min(boardLength - 1, pos + 2);
-        for (int i = start; i <= end; i++) {
-            if (i == pos) {
-                view.append(YELLOW).append("[P]").append(RESET);
-            }
-            view.append(colorTile(board[i])).append(" ");
+        String currentBoard = "|  Player  |";
+        for (int i = pos + 1; i < pos + 5; i++) {
+            currentBoard += board[i] + " ";
         }
-        System.out.println(view.toString());
+        System.out.println("Board View: " + currentBoard);
     }
 
-    // Helper that applies a color based on tile content
-    public String colorTile(String tile) {
-        if (tile.contains("+") && tile.contains("Dice"))
-            return GREEN + tile + RESET;
-        if (tile.contains("-") && tile.contains("Dice"))
-            return RED + tile + RESET;
-        if (tile.contains("Move ->"))
-            return BLUE + tile + RESET;
-        if (tile.contains("Move <-"))
-            return PURPLE + tile + RESET;
-        if (tile.contains("[") && tile.contains("]"))
-            return CYAN + tile + RESET;
-        return tile;
+
+
+
+
+    public int rollDice(){
+        return(rand.nextInt(1, 7));
     }
 
-    public int rollDice() {
-        return (rand.nextInt(1, 7));
-    }
-
-    public void generateBoard() {
-        for (int i = 0; i < boardLength; i++) {
+    public void generateBoard(){
+        for (int i = 0; i < boardLength; i ++){
             board[i] = generateTile();
         }
     }
-
-    // Used by generateBoard to generate a singular tile
-    public String generateTile() {
+    //Used by generateBoard to generate a singular tile
+    public String generateTile(){
         int randomNum;
         String tile;
         randomNum = rand.nextInt(1, 10);
-        switch (randomNum) {
+        switch (randomNum){
             case 5:
-                // extra dice tile
+                //extra dice tile
                 tile = "| +" + rand.nextInt(1, 3) + " Dice  |";
                 break;
 
             case 6:
-                // minus dice tile
+                //minus dice tile
                 tile = "| -" + rand.nextInt(1, 3) + " Dice  |";
                 break;
 
             case 7:
-                // move forward tile
+                //move forward tile
                 tile = "|Move -> " + rand.nextInt(1, 4) + " |";
                 break;
 
             case 8:
-                // move back tile
+                //move back tile
                 tile = "|Move <- " + rand.nextInt(1, 4) + " |";
                 break;
             case 9:
-                // Extra ticket tile
+                //Extra ticket tile
                 tile = "|   [ " + rand.nextInt(1, 4) + "]   |";
                 break;
             default:
-                // Empty tile
+                //Empty tile
                 tile = "|          |";
         }
-        return (tile);
+        return(tile);
     }
+
 
     public void emptySpace() {
         System.out.println("Landed on an empty tile. Nothing happens.");
     }
 
-    // Triggers when player lands on move space. Moves the user forwards or back
+    //Triggers when player lands on move space. Moves the user forwards or back
     public void moveSpace(String tile) {
         int move = Integer.parseInt(tile.replaceAll("[^0-9]", ""));
 
@@ -189,6 +136,7 @@ public class Diceopoly extends Game {
         }
     }
 
+
     public void moveDice(String tile) {
         int amount = Integer.parseInt(tile.replaceAll("[^0-9]", ""));
         if (tile.contains("+")) {
@@ -196,12 +144,15 @@ public class Diceopoly extends Game {
             diceCount += amount;
         } else if (tile.contains("-")) {
             System.out.println("Lost " + amount + " dice!");
-            if (diceCount < amount) {
+            if (diceCount < amount){
                 diceCount = 0;
-            } else {
+            }else {
                 diceCount -= amount;
             }
         }
     }
 
+
 }
+
+
