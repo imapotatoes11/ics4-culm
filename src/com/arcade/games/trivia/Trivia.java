2a6e33327a1a644ee30ce2d60634140c58d927d2
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2025-06-11 11:26:18 -0400

    Refactor ArcadeManager and Trivia classes for improved player management and question handling; update Bcolors and Wallet classes for better readability and organization.

--- src/com/arcade/games/trivia/Trivia.java
+++ src/com/arcade/games/trivia/Trivia.java
@@ -1,145 +1,82 @@
 package com.arcade.games.trivia;
+
 import com.arcade.games.Game;
 import com.arcade.item.Functional;
 import java.util.*;
 
 public class Trivia extends Game {
    private Question[] questionList = new Question[30];
-    
-   public Trivia (int id, String title, int difficulty, int requiredTokens, int ticketReward) {
+
+   // move all question text, difficulty, and answer into one place
+   private static final Object[][] QUESTION_DATA = new Object[][] {
+         { "What color is grass? \nA) Blue \nB) Green \nC) Red \nD) Yellow\n", 1, 'b' },
+         { "What vehicle can fly? \nA) Car \nB) Train \nC) Plane \nD) Submarine\n", 1, 'c' },
+         { "What is the second letter of the alphabet? \nA) C \nB) A \nC) D \nD) B\n", 1, 'd' },
+         { "Who is the main character of Kung Fu Panda? \nA) Viper \nB) Monkey \nC) Shifu \nD) Po\n", 2, 'd' },
+         { "How many novels are in the Harry Potter Series? \nA) 7 \nB) 6 \nC) 5 \nD) 8\n", 2, 'a' },
+         { "What majority color is Pikachu from Pokemon? \nA) Violet \nB) Green \nC) Yellow \nD) Black\n", 2, 'c' },
+         { "What is the biggest planet in our solar system? \nA) Mercury \nB) Sun \nC) Jupiter \nD) Earth\n", 3, 'c' },
+         { "What is the second largest country by land area? \nA) Brazil \nB) Canada \nC) USA \nD) China\n", 3, 'b' },
+         { "Who founded the tech company Apple? \nA) Elon Musk \nB) Jeff Bezos \nC) Steve Jobs \nD) Warren Buffett\n",
+               3, 'c' },
+         { "Who painted the Mona Lisa? \nA) Leonardo Davinci \nB) Pablo Picasso \nC) Bob Ross \nD) Vincent Van Gogh\n",
+               4, 'a' },
+         { "Which actor did not play Spiderman? \nA) Andrew Garfield \nB) Tobey Maguire \nC) Tom Holland \nD) Henry Cavil\n",
+               4, 'd' },
+         { "What is the most common eye color in the world? \nA) Black \nB) Blue \nC) Green \nD) Brown\n", 4, 'd' },
+         { "What country is the Taj Mahal located in? \nA) USA \nB) Brazil \nC) France \nD) India\n", 5, 'd' },
+         { "What is the capital of Italy? \nA) Venice \nB) Rome \nC) Vatican City \nD) Milan\n", 5, 'b' },
+         { "Which planet in our solar system has the most moons? \nA) Jupiter \nB) Earth \nC) Saturn \nD) Uranus\n", 5,
+               'c' },
+         { "Which of the following Celebrities are not bald? \nA) Tom Hardy \nB) Vin Diesel \nC) Dwayne Johnson \nD) Joe Rogan\n",
+               6, 'a' },
+         { "What is the only letter not used in American State? \nA) Z \nB) P \nC) X \nD) Q\n", 6, 'd' },
+         { "Which novel commences with the line Call Me Ishmael? \nA) Hamlet \nB) Percy Jackson \nC) Moby Dick \nD) Hunger Games\n",
+               6, 'c' },
+         { "Which novel features the fictional language Newspeak? \nA) Brave New World \nB) The Road \nC) 1984 \nD) Hunger Games\n",
+               7, 'c' },
+         { "What year did the Berlin Wall fall? \nA) 1988 \nB) 1989 \nC) 1990 \nD) 1991\n", 7, 'b' },
+         { "What is the name of a word that can be read the same forwards and backwards? \nA) Acroynm \nB) Emordnilap \nC) Palindrome \nD) Synonym\n",
+               7, 'c' },
+         { "What element has the highest boiling point? \nA) Tungsten \nB) Plutonium \nC) Uranium \nD) Einsteinium\n",
+               8, 'a' },
+         { "What is the oldest written language still used present day? \nA) English \nB) Hindi \nC) Arabic \nD) Chinese\n",
+               8, 'd' },
+         { "What year did the Chernobyl nuclear disaster occur? \nA) 1985 \nB) 1986 \nC) 1987 \nD) 1988\n", 8, 'b' },
+         { "What is the name of the largest moon on Saturn? \nA) Titan \nB) Colossal \nC) Alpha 6 \nD) Gamma 9\n", 9,
+               'a' },
+         { "Rounded down to the nearest minute, what is the record breathe hold time underwater? \nA) 24 minutes \nB) 18 minutes \nC) 30 minutes \nD) 16 minutes\n",
+               9, 'a' },
+         { "Rounded to the nearest meter, How tall is the CN Tower? \nA) 513 m \nB) 533 m \nC) 553 m \nD) 573 m\n", 9,
+               'c' },
+         { "What is the only continent where coffee grows naturally? \nA) Asia \nB) South America \nC) Europe \nD) Africa\n",
+               10, 'd' },
+         { "What color does gold leaf paper appear if you hold it up to a light? \nA) Still Gold \nB) Green \nC) Red \nD) Faint Blue\n",
+               10, 'b' },
+         { "How long is a Jiffy? \nA) 1 millionth of a second \nB) 1 billionth of a second \nC) 1 trillionth of a second \nD) 1 thousandth of a second\n",
+               10, 'c' },
+   };
+
+   public Trivia(int id, String title, int difficulty, int requiredTokens, int ticketReward) {
       super(id, title, difficulty, requiredTokens, ticketReward);
+      // replace manual setup with one loop
       for (int i = 0; i < questionList.length; i++) {
-          questionList[i] = new Question();
-      }      
-      questionList[0].setQuestion("What color is grass? \nA) Blue \nB) Green \nC) Red \nD) Yellow\n");
-      questionList[0].setDifficultyLevel(1);
-      questionList[0].setAnswer('b');
-      
-      questionList[1].setQuestion("What vehicle can fly? \nA) Car \nB) Train \nC) Plane \nD) Submarine\n");
-      questionList[1].setDifficultyLevel(1);
-      questionList[1].setAnswer('c');
-      
-      questionList[2].setQuestion("What is the second letter of the alphabet? \nA) C \nB) A \nC) D \nD) B\n");
-      questionList[2].setDifficultyLevel(1);
-      questionList[2].setAnswer('d');
-      
-      questionList[3].setQuestion("Who is the main character of Kung Fu Panda? \nA) Viper \nB) Monkey \nC) Shifu \nD) Po\n");
-      questionList[3].setDifficultyLevel(2);
-      questionList[3].setAnswer('d');
-      
-      questionList[4].setQuestion("How many novels are in the Harry Potter Series? \nA) 7 \nB) 6 \nC) 5 \nD) 8\n");
-      questionList[4].setDifficultyLevel(2);
-      questionList[4].setAnswer('a');
-      
-      questionList[5].setQuestion("What majority color is Pikachu from Pokemon? \nA) Violet \nB) Green \nC) Yellow \nD) Black\n");
-      questionList[5].setDifficultyLevel(2);
-      questionList[5].setAnswer('c');
-      
-      questionList[6].setQuestion("What is the biggest planet in our solar system? \nA) Mercury \nB) Sun \nC) Jupiter \nD) Earth\n");
-      questionList[6].setDifficultyLevel(3);
-      questionList[6].setAnswer('c');
-      
-      questionList[7].setQuestion("What is the second largest country by land area? \nA) Brazil \nB) Canada \nC) USA \nD) China\n");
-      questionList[7].setDifficultyLevel(3);
-      questionList[7].setAnswer('b');
-      
-      questionList[8].setQuestion("Who founded the tech company Apple? \nA) Elon Musk \nB) Jeff Bezos \nC) Steve Jobs \nD) Warren Buffett\n");
-      questionList[8].setDifficultyLevel(3);
-      questionList[8].setAnswer('c');
-      
-      questionList[9].setQuestion("Who painted the Mona Lisa? \nA) Leonardo Davinci \nB) Pablo Picasso \nC) Bob Ross \nD) Vincent Van Gogh\n");
-      questionList[9].setDifficultyLevel(4);
-      questionList[9].setAnswer('a');
-      
-      questionList[10].setQuestion("Which actor did not play Spiderman? \nA) Andrew Garfield \nB) Tobey Maguire \nC) Tom Holland \nD) Henry Cavil\n");
-      questionList[10].setDifficultyLevel(4);
-      questionList[10].setAnswer('d');
-      
-      questionList[11].setQuestion("What is the most common eye color in the world? \nA) Black \nB) Blue \nC) Green \nD) Brown\n");
-      questionList[11].setDifficultyLevel(4);
-      questionList[11].setAnswer('d');
-      
-      questionList[12].setQuestion("What country is the Taj Mahal located in? \nA) USA \nB) Brazil \nC) France \nD) India\n");
-      questionList[12].setDifficultyLevel(5);
-      questionList[12].setAnswer('d');
-      
-      questionList[13].setQuestion("What is the capital of Italy? \nA) Venice \nB) Rome \nC) Vatican City \nD) Milan\n");
-      questionList[13].setDifficultyLevel(5);
-      questionList[13].setAnswer('b');
-      
-      questionList[14].setQuestion("Which planet in our solar system has the most moons? \nA) Jupiter \nB) Earth \nC) Saturn \nD) Uranus\n");
-      questionList[14].setDifficultyLevel(5);
-      questionList[14].setAnswer('c');
-      
-      questionList[15].setQuestion("Which of the following Celebrities are not bald? \nA) Tom Hardy \nB) Vin Diesel \nC) Dwayne Johnson \nD) Joe Rogan\n");
-      questionList[15].setDifficultyLevel(6);
-      questionList[15].setAnswer('a');
-      
-      questionList[16].setQuestion("What is the only letter not used in American State? \nA) Z \nB) P \nC) X \nD) Q\n");
-      questionList[16].setDifficultyLevel(6);
-      questionList[16].setAnswer('d');
-      
-      questionList[17].setQuestion("Which novel commences with the line Call Me Ishmael? \nA) Hamlet \nB) Percy Jackson \nC) Moby Dick \nD) Hunger Games\n");
-      questionList[17].setDifficultyLevel(6);
-      questionList[17].setAnswer('c');
-      
-      questionList[18].setQuestion("Which novel features the fictional language Newspeak? \nA) Brave New World \nB) The Road \nC) 1984 \nD) Hunger Games\n");
-      questionList[18].setDifficultyLevel(7);
-      questionList[18].setAnswer('c');
-      
-      questionList[19].setQuestion("What year did the Belin Wall fall? \nA) 1988 \nB) 1989 \nC) 1990 \nD) 1991\n");
-      questionList[19].setDifficultyLevel(7);
-      questionList[19].setAnswer('b');
-      
-      questionList[20].setQuestion("What is the name of a word that can be read the same forwards and backwards? \nA) Acroynm \nB) Emordnilap \nC) Palindrome \nD) Synonym\n");
-      questionList[20].setDifficultyLevel(7);
-      questionList[20].setAnswer('c');
-      
-      questionList[21].setQuestion("What element has the highest boiling point? \nA) Tungsten \nB) Plutonium \nC) Uranium \nD) Einsteinium\n");
-      questionList[21].setDifficultyLevel(8);
-      questionList[21].setAnswer('a');
-      
-      questionList[22].setQuestion("What is the oldest written language still used present day? \nA) English \nB) Hindi \nC) Arabic \nD) Chinese\n");
-      questionList[22].setDifficultyLevel(8);
-      questionList[22].setAnswer('d');
-      
-      questionList[23].setQuestion("What year did the Chernobyl nuclear disaster occur? \nA) 1985 \nB) 1986 \nC) 1987 \nD) 1988\n");
-      questionList[23].setDifficultyLevel(8);
-      questionList[23].setAnswer('b');
-      
-      questionList[24].setQuestion("What is the name of the largest moon on saturn? \nA) Titan \nB) Colossal \nC) Alpha 6 \nD) Gamma 9\n");
-      questionList[24].setDifficultyLevel(9);
-      questionList[24].setAnswer('a');
-      
-      questionList[25].setQuestion("Rounded down to the nearest minute, what is the record breathe hold time underwater? \nA) 24 minutes \nB) 18 minutes \nC) 30 minutes \nD) 16 minutes\n");
-      questionList[25].setDifficultyLevel(9);
-      questionList[25].setAnswer('a');
-      
-      questionList[26].setQuestion("Rounded to the nearest meter, How tall is the CN Tower? \nA) 513 m \nB) 533 m \nC) 553 m \nD) 573 m\n");
-      questionList[26].setDifficultyLevel(9);
-      questionList[26].setAnswer('c');
-      
-      questionList[27].setQuestion("What is the only continent where coffee grows naturally? \nA) Asia \nB) South America \nC) Europe \nD) Africa\n");
-      questionList[27].setDifficultyLevel(10);
-      questionList[27].setAnswer('d');
-      
-      questionList[28].setQuestion("What color does gold leaf paper appear if you hold it up to a light? \nA) Still Gold \nB) Green \nC) Red \nD) Faint Blue\n");
-      questionList[28].setDifficultyLevel(10);
-      questionList[28].setAnswer('b');
-      
-      questionList[29].setQuestion("How long is a Jiffy? \nA) 1 millionth of a second \nB) 1 billionth of a second \nC) 1 trillionth of a second \nD) 1 thousandth of a second\n");
-      questionList[29].setDifficultyLevel(10);
-      questionList[29].setAnswer('c');
+         questionList[i] = new Question();
+         Object[] d = QUESTION_DATA[i];
+         questionList[i].setQuestion((String) d[0]);
+         questionList[i].setDifficultyLevel((Integer) d[1]);
+         questionList[i].setAnswer((Character) d[2]);
+      }
    }
-      
-   public Question[] getQuestionList () {
-      return questionList;   
+
+   public Question[] getQuestionList() {
+      return questionList;
    }
-   
-   public int promptQuestion () {
+
+   public int promptQuestion() {
       char chosenAnswer;
       int count = 0;
-      Scanner sc = new Scanner (System.in);
+      Scanner sc = new Scanner(System.in);
       for (int i = 0; i < questionList.length; i++) {
          if (questionList[i].getDifficultyLevel() == getDifficulty()) {
             System.out.print(questionList[i].getQuestion());
@@ -148,46 +85,41 @@ public class Trivia extends Game {
             if (chosenAnswer == questionList[i].getAnswer()) {
                if (questionList[i].getDifficultyLevel() <= 3) {
                   count += 1;
-               }
-               else if (questionList[i].getDifficultyLevel() <= 6) {
+               } else if (questionList[i].getDifficultyLevel() <= 6) {
                   count += 3;
-               }
-               else {
+               } else {
                   count += 5;
                }
-            }
-            else {
+            } else {
                if (questionList[i].getDifficultyLevel() <= 3) {
                   count -= 1;
-               }
-               else if (questionList[i].getDifficultyLevel() <= 6) {
+               } else if (questionList[i].getDifficultyLevel() <= 6) {
                   count -= 3;
-               }
-               else {
+               } else {
                   count -= 5;
                }
             }
          }
       }
-      
+
       return count;
    }
-   
-   public int runGame (ArrayList<Functional> useItems) {
+
+   public int runGame(ArrayList<Functional> useItems) {
       int originalReward = super.getTicketReward();
-      
+
       System.out.println("Welcome to Trivia!");
       System.out.println("Your chosen difficulty is " + getDifficulty() + ". Here are your 3 questions:");
-      
+
       int gameOutcome = promptQuestion();
-      
+
       return gameOutcome + originalReward;
    }
-   
+
    public static void main(String[] args) {
-        Trivia game = new Trivia( 3, "Trivia", 5, 2, 10);
-        ArrayList<Functional> items = new ArrayList<>();  // no items used here
-        int earned = game.runGame(items);
-        System.out.printf("Game Over! You've earned %d tickets!", earned);
-    }
+      Trivia game = new Trivia(3, "Trivia", 5, 2, 10);
+      ArrayList<Functional> items = new ArrayList<>(); // no items used here
+      int earned = game.runGame(items);
+      System.out.printf("Game Over! You've earned %d tickets!", earned);
+   }
 }
--- src/com/arcade/util/Bcolors.java
+++ src/com/arcade/util/Bcolors.java
@@ -20,62 +20,64 @@
 package com.arcade.util;
 
 public class Bcolors {
-    public static final String HEADER = "\033[95m";
-    public static final String OKBLUE = "\033[94m";
-    public static final String OKCYAN = "\033[96m";
-    public static final String OKGREEN = "\033[92m";
-    public static final String WARNING = "\033[93m";
-    public static final String FAIL = "\033[91m";
-    public static final String ENDC = "\033[0m";
-    public static final String BOLD = "\033[1m";
-    public static final String UNDERLINE = "\033[4m";
+    // JGRASP DOESN'T SUPPORT ANSI COLORS, UNCOMMENT TO USE
+
+    public static final String HEADER = "";// "\033[95m";
+    public static final String OKBLUE = "";// "\033[94m";
+    public static final String OKCYAN = "";// "\033[96m";
+    public static final String OKGREEN = "";// "\033[92m";
+    public static final String WARNING = "";// "\033[93m";
+    public static final String FAIL = "";// "\033[91m";
+    public static final String ENDC = "";// "\033[0m";
+    public static final String BOLD = "";// "\033[1m";
+    public static final String UNDERLINE = "";// "\033[4m";
 
     // Text colors
-    public static final String BLACK = "\033[30m";
-    public static final String RED = "\033[31m";
-    public static final String GREEN = "\033[32m";
-    public static final String YELLOW = "\033[33m";
-    public static final String BLUE = "\033[34m";
-    public static final String MAGENTA = "\033[35m";
-    public static final String CYAN = "\033[36m";
-    public static final String WHITE = "\033[37m";
+    public static final String BLACK = "";// "\033[30m";
+    public static final String RED = "";// "\033[31m";
+    public static final String GREEN = "";// "\033[32m";
+    public static final String YELLOW = "";// "\033[33m";
+    public static final String BLUE = "";// "\033[34m";
+    public static final String MAGENTA = "";// "\033[35m";
+    public static final String CYAN = "";// "\033[36m";
+    public static final String WHITE = "";// "\033[37m";
 
     // Bright text colors
-    public static final String BRIGHT_BLACK = "\033[90m";
-    public static final String BRIGHT_RED = "\033[91m";
-    public static final String BRIGHT_GREEN = "\033[92m";
-    public static final String BRIGHT_YELLOW = "\033[93m";
-    public static final String BRIGHT_BLUE = "\033[94m";
-    public static final String BRIGHT_MAGENTA = "\033[95m";
-    public static final String BRIGHT_CYAN = "\033[96m";
-    public static final String BRIGHT_WHITE = "\033[97m";
+    public static final String BRIGHT_BLACK = "";// "\033[90m";
+    public static final String BRIGHT_RED = "";// "\033[91m";
+    public static final String BRIGHT_GREEN = "";// "\033[92m";
+    public static final String BRIGHT_YELLOW = "";// "\033[93m";
+    public static final String BRIGHT_BLUE = "";// "\033[94m";
+    public static final String BRIGHT_MAGENTA = "";// "\033[95m";
+    public static final String BRIGHT_CYAN = "";// "\033[96m";
+    public static final String BRIGHT_WHITE = "";// "\033[97m";
 
     // Background colors
-    public static final String BG_BLACK = "\033[40m";
-    public static final String BG_RED = "\033[41m";
-    public static final String BG_GREEN = "\033[42m";
-    public static final String BG_YELLOW = "\033[43m";
-    public static final String BG_BLUE = "\033[44m";
-    public static final String BG_MAGENTA = "\033[45m";
-    public static final String BG_CYAN = "\033[46m";
-    public static final String BG_WHITE = "\033[47m";
+    public static final String BG_BLACK = "";// "\033[40m";
+    public static final String BG_RED = "";// "\033[41m";
+    public static final String BG_GREEN = "";// "\033[42m";
+    public static final String BG_YELLOW = "";// "\033[43m";
+    public static final String BG_BLUE = "";// "\033[44m";
+    public static final String BG_MAGENTA = "";// "\033[45m";
+    public static final String BG_CYAN = "";// "\033[46m";
+    public static final String BG_WHITE = "";// "\033[47m";
 
     // Bright background colors
-    public static final String BG_BRIGHT_BLACK = "\033[100m";
-    public static final String BG_BRIGHT_RED = "\033[101m";
-    public static final String BG_BRIGHT_GREEN = "\033[102m";
-    public static final String BG_BRIGHT_YELLOW = "\033[103m";
-    public static final String BG_BRIGHT_BLUE = "\033[104m";
-    public static final String BG_BRIGHT_MAGENTA = "\033[105m";
-    public static final String BG_BRIGHT_CYAN = "\033[106m";
-    public static final String BG_BRIGHT_WHITE = "\033[107m";
+    public static final String BG_BRIGHT_BLACK = "";// "\033[100m";
+    public static final String BG_BRIGHT_RED = "";// "\033[101m";
+    public static final String BG_BRIGHT_GREEN = "";// "\033[102m";
+    public static final String BG_BRIGHT_YELLOW = "";// "\033[103m";
+    public static final String BG_BRIGHT_BLUE = "";// "\033[104m";
+    public static final String BG_BRIGHT_MAGENTA = "";// "\033[105m";
+    public static final String BG_BRIGHT_CYAN = "";// "\033[106m";
+    public static final String BG_BRIGHT_WHITE = "";// "\033[107m";
 
     // Text styles
-    public static final String DIM = "\033[2m";
-    public static final String ITALIC = "\033[3m";
-    public static final String BLINK = "\033[5m";
-    public static final String REVERSE = "\033[7m";
-    public static final String STRIKETHROUGH = "\033[9m";
+    public static final String DIM = "";// "\033[2m";
+    public static final String ITALIC = "";// "\033[3m";
+    public static final String BLINK = "";// "\033[5m";
+    public static final String REVERSE = "";// "\033[7m";
+    public static final String STRIKETHROUGH = "";// "\033[9m";
 
     // Bold combinations
     public static final String BOLD_BLACK = BOLD + BLACK;
